bd8db6405e234d646a0a983b41fc665c34f3e5bc
Amy Lopez <amylopezdev@gmail.com>
2026-04-25 16:25:58 +0100

    Implement Basic Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import Header from './components/Header';
 import HomePage from "./pages/HomePage";
 import MovieDetailPage from "./pages/MovieDetailPage";
 import WatchlistPage from "./pages/WatchlistPage";
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/movie/:id" element={<MovieDetailPage />} />
-    <Route path="/watchlist" element={<WatchlistPage />} />
-  </Routes>
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MovieDetailPage />} />
+      <Route path="/watchlist" element={<WatchlistPage />} />
+    </Routes>
+  </>
 );
 
 export default App;
