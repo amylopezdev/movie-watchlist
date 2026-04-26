b73d71e9d2ffd3604190338e40deb7d7a2b6e647
Amy Lopez <amylopezdev@gmail.com>
2026-04-26 15:49:52 +0100

    Fetch Trending Movies On Homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,24 @@
-const HomePage = () => <h1>Home</h1>;
+import { useState, useEffect } from "react";
+import { getTrendingMovies } from "../api/tmdb";
+import type { Movie } from "../types/movie";
+
+const HomePage = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const data = await getTrendingMovies();
+        setMovies(data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return <h1>Home</h1>;
+};
 
 export default HomePage;
