9fd8bdae544b083364a5c8250cb8fa50aa1e82ed
Amy Lopez <amylopezdev@gmail.com>
2026-04-26 11:39:16 +0100

    Implement “Get Trending Movies” API Function

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,2 +1,15 @@
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+export async function getTrendingMovies() {
+  const response = await fetch(
+    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch trending movies");
+  }
+
+  const data = await response.json();
+  return data;
+}
